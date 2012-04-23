faa15ae94492c8fd06671d7ea7253004fcd0667b
Sean Stephenson <seanmichaelstephenson@gmail.com>
2012-04-22 21:00:27 -0700

    update snag to write to console and give more space btween links

--- snag.js
+++ snag.js
@@ -1,12 +1,10 @@
-var img = document.getElementByClassName("BVks2c tJOOi")[0];
-var src = img.src.replace(/s[0-9]+/,"s640");
-alert("<table><img src="+src"></table>");
-
-alert("<table style="width:auto;"><tr><td><a href=""><img src="" height="480" width="640" /></a></td></tr><tr><td style="font-family:arial,sans-serif; font-size:11px; text-align:right">From <a href="https://picasaweb.google.com/104608116512003739301/Spring2012?authuser=0&feat=embedwebsite">spring2012</a></td></tr></table>");
-
-var container = document.getElementsByClassName("RazREb ZI35oe")[0];
+var container = document.getElementsByClassName("IFU6qd tNsA5e-yd")[0]
+container = container.getElementsByClassName("s-W-Oh")[0];
 var imgs = container.getElementsByTagName("img");
-var srcs = [];
+var links = "";
+var album = document.URL;
 for (var i=0,l=imgs.length; i<l; i++) {
-  srcs.append(imgs[i].src.replace(/w[0-9]+-h[0-9]+[^\/]/, "w640-h480"));
+  var src = imgs[i].src.replace(/w[0-9]+-h[0-9]+[^\/]+/, "w640-h480");
+  links += "<a href="+album+"><img style='padding: 10px 0;' src="+src+" /></a>\n";
 }
+alert(links);
