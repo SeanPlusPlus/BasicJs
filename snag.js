af873da0f2a0c54ef3b502050e982b33c91945e9
Sean Stephenson <seanmichaelstephenson@gmail.com>
2012-04-22 21:06:34 -0700

    update snag to log correctcly

--- snag.js
+++ snag.js
@@ -5,6 +5,6 @@ var links = "";
 var album = document.URL;
 for (var i=0,l=imgs.length; i<l; i++) {
   var src = imgs[i].src.replace(/w[0-9]+-h[0-9]+[^\/]+/, "w640-h480");
-  links += "<a href="+album+"><img style='padding: 10px 0;' src="+src+" /></a>\n";
+  links += "<a href="+album+"><img style='padding: 10px 0;' src="+src+" /></a>\n\n\n\n";
 }
-alert(links);
+console.log(links);
